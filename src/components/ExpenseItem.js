7e49b8dd082c097a4a0401eb3ecb417670b6fcda
Tim <dtimcockerill2@gmail.com>
2021-04-29 16:24:39 -0400

    created ExpenseDate component and style sheet

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+// import userEvent from '@testing-library/user-event';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
+   
     return (
         <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
+            <ExpenseDate date = {props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
