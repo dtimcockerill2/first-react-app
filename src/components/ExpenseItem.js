88b34c1b66b59e099036317eeb35df9526be72aa
Tim <dtimcockerill2@gmail.com>
2021-05-05 00:45:12 -0400

    added button, onClick event handler

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,8 @@ import Card from './Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-   
+   const clickHandler = () => console.log("clicked!");
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date = {props.date}/>
@@ -12,6 +13,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>);
 }
 
